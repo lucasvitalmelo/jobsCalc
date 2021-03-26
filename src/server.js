2a72ef5552a0558ce77e98e14595f36e827d755a
Lucas Vital <lucasvitaldemelo@gmail.com>
2021-03-26 17:41:58 -0300

    server-json

---
+++ src/server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const server = express()
+
+// request, response
+server.get('/', (request, response) => {
+    return response.sendFile(__dirname + "/views/index.html")
+})
+
+server.listen(3000, () => console.log('rodando'))

beb21030d99a2690cfda8a767072a45a973333de
lucas vital <lucasvitaldemelo@gmail.com>
2021-07-26 17:47:10 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router()
 
 const profile = {
     name:"Lucas",
-    avatar: "https://avatars.githubusercontent.com/u/71709874?v=4",
+    avatar: "https:github.com/lucasvitalmelo.png",
     "monthly-budget":3000,
     "days-per-week":5,
     "hours-per-day":5,
@@ -15,6 +15,10 @@ const views = __dirname + "/views/"
 // request, response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) => res.render(views + "job"))
+routes.post('/job', (req, res) => {
+    console.log(req.body)
+})
+
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", {profile}))
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+// usando tamplate engine
 server.set('view engine', 'ejs')
 
 // habilitar arquivos statics
